24e376401a8b6464a18124c730914af058688f64
Han Feng <hanf@seagroup.com>
2019-11-24 22:16:20 +0800

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Han Feng on 11/24/19.
+//  Copyright © 2019 Han Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
